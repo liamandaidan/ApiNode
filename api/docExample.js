297b2a687eabbef20363deb2d7a0f9cf5d44ac04
Liam <liammclaughlinis@gmail.com>
2022-08-31 16:20:42 -0600

    bet

---
+++ api/docExample.js
@@ -0,0 +1,36 @@
+/**
+ * Student name
+ * @type {string}
+ */
+const studentName = 'sdaj asd';
+
+/**
+ * Grade array
+ * @type {Array<number>}
+ */
+const grades = [123, 23.4, 21.5];
+
+/**
+ * @type {{id: number, text: string|number}}
+ */
+const idk = {
+    id: 1,
+    text: 'hi'
+}
+
+/**
+ * This is hello world!
+ * @param {string}
+ */
+function helloWorld(input){
+    
+}
+/**
+ * Random function woot!
+ * @param {number} amount - Total amt
+ * @param {number} cool - Item to add
+ * @returns {string} - Total
+ */
+const otherFunc = (amount, cool) => {
+    return `${amount+cool}`;
+}
